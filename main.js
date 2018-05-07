d6b1e760879ddb538b7fa894b9bbe83b719dc808
stavl <stavlev95@gmail.com>
2018-05-07 12:42:45 +0300

    Truly turned main.js to ES6 using arrow and shorthand functions, template strings, and spread and rest operators

--- main.js
+++ main.js
@@ -1,75 +1,37 @@
-const heroesJson = loadJson();
+const {names, roles, hp} = require('./ow.json');
 
-console.log(heroesJson);
+const getHeroes = () => names.map((name, currIndex) => ({name, role: roles[currIndex], hp: hp[currIndex]}));
+
+const getByRoles = (heroes, ...requestedRoles) => (
+    heroes.filter(hero => requestedRoles.includes(hero.role))
+);
+
+const groupBy = (array, propToGroupBy = 'role') => {
+    return array.reduce((groupedByArray, currElement) => {
+        groupedByArray[currElement[propToGroupBy]] = groupedByArray[currElement[propToGroupBy]] || [];
+        (groupedByArray[currElement[propToGroupBy]]).push(currElement); // Push the hero object to the cell in the result array which has the selected key
+
+        return groupedByArray;
+    }, []);
+};
+
+const makeHeroesNice = (heroes) => {
+    return heroes.map((currHero, currIndex) => ({
+            ...currHero,
+            sayHello: () => {
+                console.log(`Hi! My name is ${currHero.name}, nice to meet you!`)
+            }
+        })
+    );
+};
 
 const heroes = getHeroes();
 
 console.log(groupBy(heroes));
 console.log(groupBy(heroes, 'name'));
 
-console.log(getByRoles(heroes, ['Offense']));
-console.log(getByRoles(heroes, ['Support', 'Offense']));
+console.log(getByRoles(heroes, 'Offense'));
+console.log(getByRoles(heroes, 'Support', 'Offense'));
 
 const niceHeroes = makeHeroesNice(heroes);
 niceHeroes[0].sayHello();
-
-function loadJson() {
-    const jsonFile = require('./ow.json');
-    return jsonFile;
-};
-
-function getHeroes() {
-    const names = heroesJson.names;
-    const roles = heroesJson.roles;
-
-    const heroes = names.map((currName, currIndex) => {
-        return {
-            name: currName,
-            role: roles[currIndex]
-        };
-    });
-
-    return heroes;
-};
-
-function groupBy(array, propToGroupBy) {
-    const resultArray = array.reduce((groupedByArray, currElement) => {
-        propToGroupBy = propToGroupBy || 'role';
-
-        const currValOfPropToGroupBy = currElement[propToGroupBy]; // Equivalent to 'currElement.role' (or 'currElement.name' etc.)
-        const currValOfKeyToGroupBy = groupedByArray[currValOfPropToGroupBy];
-
-        if (!currValOfKeyToGroupBy) {
-            groupedByArray[currValOfPropToGroupBy] = [];
-        }
-
-        (groupedByArray[currValOfPropToGroupBy]).push(currElement); // Push the hero object to the cell in the result array which has the selected key
-
-        return groupedByArray;
-    }, []);
-
-    return resultArray;
-};
-
-function getByRoles(array, requestedRoles) {
-    return array.filter(element => {
-        const doesElementHaveAnyOfTheRequestedRoles = requestedRoles.some(requestedRole => {
-            return requestedRole === element.role;
-        });
-
-        if (doesElementHaveAnyOfTheRequestedRoles) {
-            return element;
-        }
-    });
-};
-
-function makeHeroesNice(heroes) {
-    heroes.forEach(hero =>
-       hero.sayHello = () => {
-           console.log("Hi! My name is " + hero.name + ", nice to meet you!");
-       }
-    );
-
-    return heroes;
-};
-
